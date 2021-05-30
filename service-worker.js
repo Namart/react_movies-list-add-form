1c7d826ff5a185c9d3eb506c7cfa68efa437502a
Anastasiya <anastasiya.triumf@gmail.com>
2021-05-30 21:52:24 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2088c32903613710f8f9678a01b40cd4.js"
+  "./precache-manifest.de9e1ab3b24722f6a1c91d18586fdc64.js"
 );
 
 self.addEventListener('message', (event) => {
